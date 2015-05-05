d0612e01a0e3574b99767d2d518fb6db35817804
chudur-budur <khaled.ahsan.talukder@gmail.com>
2015-05-05 19:50:14 -0400

    yo

--- src/simulator/treeParser.py
+++ src/simulator/treeParser.py
@@ -6,20 +6,20 @@ Created on Apr 30, 2015
 
 
 class Node:
+
+    operator = None
+    operands = None
     
     def __init__(self):
-        self.instruction = None
-        self.arguments = []
-        #self.children = []
+        self.operator = None
+        self.operands = []
         self.parent = None
     
-    """
     def __str__(self):
-        return "("+str(self.instruction)+","+str(self.arguments)+")"
+        return str(self.operator) 
     
     def __repr__(self):
-        return "("+str(self.instruction)+","+str(self.arguments)+")"
-    """
+        return str(self.operator) 
         
 class Parser:
     '''
@@ -28,6 +28,43 @@ class Parser:
     
     def __init__(self):
         pass
+
+    def flatten(self, treeString):
+        # flatten the string
+        treeString = " ".join(treeString.split())
+        treeString = treeString.replace("(","( ")
+        treeString = treeString.replace(")"," )")
+        
+        processed = treeString.split()
+        return processed
+
+    def pop_until(self, stack, end):
+        popped = ""
+        lst = []
+        while (popped != end):
+            popped = stack.pop(0)
+            lst.append(popped)
+    
+        lst.pop()
+        return lst
+
+    def do_parse(self, treeString):
+        tokenList = self.flatten(treeString)
+        print tokenList
+
+        stack = []
+        stack.append(tokenList[0])
+        print stack
+
+        i = 1
+        while (not stack):
+            if tokenList[i] == ")":
+               popped = self.pop_until(stack, "(")               
+               node = Node()
+               node.operator = popped.pop(0)
+               node.operands = popped 
+            else
+                stack.append(tokenList[i])
     
     def parse_tree(self, treeString):
         # flatten the string
@@ -36,9 +73,10 @@ class Parser:
         treeString = treeString.replace(")"," )")
         
         processed = treeString.split()
+        print processed
         
         root = Node()
-        root.instruction = "root"
+        root.operator = "root"
         currNode = root
         prevItem = None
         
@@ -47,18 +85,19 @@ class Parser:
                 # new node
                 newNode = Node()
                 newNode.parent = currNode
-                currNode.arguments.append(newNode) # add the new node as an argument (child) of the current node
+                # add the new node as an argument (child) of the current node
+                currNode.operands.append(newNode) 
                 currNode = newNode
                 
             elif item != "(" and item != ")" and prevItem == "(":
-                # instruction
-                currNode.instruction = item
+                # operator
+                currNode.operator = item
             
             elif item != "(" and item != ")" and prevItem != "(":
                 # argument
                 newNode = Node()
-                newNode.instruction = item
-                currNode.arguments.append(newNode)
+                newNode.operator = item
+                currNode.operands.append(newNode)
             
             elif item == ")":
                 # finished node
@@ -68,27 +107,37 @@ class Parser:
             
         return root
 
+    def __str__(self):
+        return str(root)
 
 if __name__ == "__main__":
     # test if the parser works...
     
-    testTree = '''
-    (if in-cs (set-cs false))
-    (if want-cs 
-        (if has-token (progn
-            (set-cs true)
-            (set-want-cs false)
-            (set-reg-req false)
-            (pass))))
     '''
-    treeTest = '''
+    testTree = """
+    (progn2
+        (if in-cs (set-cs false))
+        (if want-cs 
+            (if has-token 
+                (progn2
+                    (set-cs true)
+                    (progn2
+                        (set-want-cs false)
+                        (set-reg-req false))))))
+    """
+    '''
+
+    '''
+    testTree = """
  (if-else (not in-cs) (if-else (not in-cs)
      set-cs-true (progn2 set-cs-true set-cs-true))
      (if-else (not in-cs) (if-else in-cs set-cs-true
          set-cs-true) (progn2 set-cs-true set-cs-true)))
+"""
 '''
     
-    treeTest = '''
+    '''
+    testTree = """
  (progn2 (if-else (and (not (and in-cs in-cs))
      (not (not in-cs))) (if-else (and (and in-cs
      in-cs) (not in-cs)) (set-cs true) (if (and
@@ -98,14 +147,25 @@ if __name__ == "__main__":
          in-cs (no-op) (no-op)) (if-else in-cs (no-op)
          (no-op))) (progn2 (progn2 (no-op) (no-op))
          (progn2 (no-op) (no-op)))) (set-cs true)))
+"""
 '''
-    
 
+    testTree = "(- (+ x y) (* a b))"
 
     parser = Parser()
-    tree = parser.parse_tree(testTree)
-    print tree
-    print "finished"
+    stack = ["a", "b", "c", ")", "d"]
+    stack.append("e")
+    print "stack: " + str(stack)
+    popped = parser.pop_until(stack, ")")
+    print "popped: " + str(popped)
+    print "stack after pop: " + str(stack)
+
+    # parser = Parser()
+    # tree = parser.parse_tree(testTree)
+    # print tree
+    # tree = parser.do_parse(testTree)
+    # print tree
+    # print "finished"
     
     pass
     
